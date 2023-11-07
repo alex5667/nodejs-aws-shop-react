d399e9803ff1ba2875cd3d2ea0b3753e25366d7c
alex5667 <alex56671111@gmail.com>
2023-11-07 05:00:52 +0200

    feat: add invalidation

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -3,22 +3,23 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as deployment from "aws-cdk-lib/aws-s3-deployment";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as cforigin from "aws-cdk-lib/aws-cloudfront-origins";
+import * as customResources from "aws-cdk-lib/custom-resources";
 
 const app = new cdk.App();
 
-const stack = new cdk.Stack(app, "ShopStack", {
+const stack = new cdk.Stack(app, "MyShopStack", {
   env: { region: "eu-west-1" },
 });
 
-const bucket = new s3.Bucket(stack, "ShopBucket", {
-  bucketName: "nodejs-aws-shop",
+const bucket = new s3.Bucket(stack, "MyShopBucket", {
+  bucketName: "nodejs-aws-myshop",
   removalPolicy: cdk.RemovalPolicy.DESTROY,
   autoDeleteObjects: true,
 });
 
 const accessIdentity = new cloudfront.OriginAccessIdentity(
   stack,
-  "ShopBucketIdentity",
+  "MyShopBucketIdentity",
   {
     comment: bucket.bucketName,
   }
@@ -30,7 +31,7 @@ const cfOrigin = new cforigin.S3Origin(bucket, {
   originAccessIdentity: accessIdentity,
 });
 
-const cf = new cloudfront.Distribution(stack, "ShopDistribution", {
+const cf = new cloudfront.Distribution(stack, "MyShopDistribution", {
   defaultBehavior: {
     origin: cfOrigin,
     viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
@@ -51,5 +52,38 @@ new deployment.BucketDeployment(stack, "DeployApp", {
   distribution: cf,
   distributionPaths: ["/*"],
 });
-cdk.Tags.of(stack).add("delete-on-destroy", "true");
-cdk.Tags.of(cf).add("delete-on-destroy", "true");
+
+const invalidation = new customResources.AwsCustomResource(
+  stack,
+  "Invalidation",
+  {
+    onCreate: {
+      service: "CloudFront",
+      action: "createInvalidation",
+      parameters: {
+        DistributionId: cf.distributionId,
+        InvalidationBatch: {
+          CallerReference: Date.now().toString(),
+          Paths: { Quantity: 1, Items: ["/*"] },
+        },
+      },
+      physicalResourceId:
+        customResources.PhysicalResourceId.of("InvalidationId"),
+    },
+    policy: customResources.AwsCustomResourcePolicy.fromSdkCalls({
+      resources: customResources.AwsCustomResourcePolicy.ANY_RESOURCE,
+    }),
+  }
+);
+
+invalidation.node.addDependency(cf);
+
+new cdk.CfnOutput(stack, "СloudFrontURL", {
+  value: cf.distributionDomainName,
+  description: "The URL of the cloudfront",
+});
+
+new cdk.CfnOutput(stack, "bucketUrl", {
+  value: bucket.bucketWebsiteUrl,
+  description: "The URL of the bucket",
+});
